c66e92b0a6bda69da290316a908099d03d0ed263
payaan2018 <37317503+payaan2018@users.noreply.github.com>
2020-04-08 14:49:59 +0800

    Add files via upload
    my first commit...

---
+++ TestMemory.java
@@ -0,0 +1,10 @@
+package com.zhy;
+
+public class TestMemory {
+    public static void main(String[] args) {
+        long maxMemory = Runtime.getRuntime().maxMemory();//返回Java 虚拟机试图使用的最大内存量。
+        long totalMemory = Runtime.getRuntime().totalMemory();//返回Java 虚拟机中的内存总量。
+        System.out.println("MAX_MEMORY = " + maxMemory + "（字节）、" + (maxMemory / (double) 1024 / 1024) + "MB");
+        System.out.println("TOTAL_MEMORY = " + totalMemory + "（字节）、" + (totalMemory / (double) 1024 / 1024) + "MB");
+    }
+}
